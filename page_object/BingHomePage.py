9da011b97e07727bfaf886a536533ad976ba9a1a
huian212 <huian@thoughtworks.com>
2019-11-25 17:12:46 +0800

    Update BingHomePage.py

--- page_object/BingHomePage.py
+++ page_object/BingHomePage.py
@@ -1,15 +1,16 @@
+# 打开页面
 def open(context):
     context.driver.get('http://www.bing.com')
     context.driver.maximize_window()
 
-
+# Bing搜索框元素
 def search_textbox(context):
     return context.driver.find_element_by_id('sb_form_q')
 
-
+# 提交按钮
 def submit_button(context):
     return context.driver.find_element_by_id('sb_go_par')
 
-
+# 登录链接
 def sign_in_link(context):
     return context.driver.find_element_by_id('id_s')
