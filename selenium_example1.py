02c57d79dac4bf9188f0e2dbbd97cb0391d5a16e
huian212 <huian@thoughtworks.com>
2019-11-26 17:57:13 +0800

    Update selenium_example1.py

--- selenium_example1.py
+++ selenium_example1.py
@@ -1,9 +1,16 @@
 import chromedriver_binary as chrome
 from selenium import webdriver
 
+# 初始化浏览器，不同浏览器类型参考Selenium文档
 driver = webdriver.Chrome(chrome.chromedriver_filename)
 # driver = webdriver.Firefox(executable_path = "driver/geckodriver")
+
+# 打开网址
 driver.get("http://www.bing.com")
+
+# 最大化窗口
 driver.maximize_window()
+
+# 关闭窗口
 driver.quit()
 # driver.close()
