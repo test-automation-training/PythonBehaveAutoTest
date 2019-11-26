05c62179c65dc0e6bdbe4430ac605dde0a3633fc
huian212 <huian@thoughtworks.com>
2019-11-26 17:54:26 +0800

    Update Login.py

--- steps/Login.py
+++ steps/Login.py
@@ -7,6 +7,7 @@ import page_object.SignInPage as SignInPage
 
 @Step('我已经登录到系统中')
 def step_impl(context):
+    # 登录步骤
     BingHomePage.sign_in_link(context).click()
     SignInPage.username_textbox(context).clear()
     SignInPage.username_textbox(context).send_keys("pollyan212@hotmail.com")
