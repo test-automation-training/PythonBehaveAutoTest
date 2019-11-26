7cf3bd519c30f27e45201f78df31646b88957cf5
huian212 <huian@thoughtworks.com>
2019-11-26 18:02:42 +0800

    Update selenium_example3.py

--- selenium_example3.py
+++ selenium_example3.py
@@ -10,7 +10,7 @@ driver.maximize_window()
 # driver.save_screenshot('screenshot/homepage.png')
 
 driver.find_element_by_id("sb_form_q").send_keys("game")
-
+# 下面是等待机制，等待某个元素满足特定条件
 # WebDriverWait(driver,10).until(EC.presence_of_all_elements_located)
 # WebDriverWait(driver,10).until(EC.element_to_be_clickable((By.ID, "sb_go_par")))
 
